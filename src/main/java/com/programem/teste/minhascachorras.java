19df157aa4f1ecffc2cacb3a0e61b2716dbf6568
marcusportela <marcustalegal.mp@gmail.com>
2020-07-17 08:50:12 -0300

    Resolução de bugs

--- src/main/java/com/programem/teste/minhascachorras.java
+++ src/main/java/com/programem/teste/minhascachorras.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class minhascachorras {
     @GetMapping("/minhascachorras")
-    public String minhascachorras(){
+    public String Minhascachorras(){
         return "3minhascachorras";
     }
 }
